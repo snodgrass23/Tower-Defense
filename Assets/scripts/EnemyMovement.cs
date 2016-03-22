b6e3877ef582b56c72db1254fd5d4e55df77ba8a
Jim Snodgrass <snodgrass23@gmail.com>
2016-03-21 22:13:30 -0400

    add start of enemy health script AGAIN

---
+++ Assets/scripts/EnemyMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour {
+
+	Transform player;
+	NavMeshAgent nav;
+
+	float timeBetweenFindingTarget = 1f;
+	float timer;
+
+	void Awake () {
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+		nav = GetComponent <NavMeshAgent> ();
+		nav.SetDestination (player.position);
+	}
+
+
+	void Update () {
+		timer += Time.deltaTime;
+
+		if (timer > timeBetweenFindingTarget) {
+			timer = 0f;
+			SetTarget ();
+		}
+	}
+
+	void SetTarget () {
+		//TODO update to choose to attack towers, etc
+		nav.SetDestination (player.position);
+	}
+
+}
---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public Transform[] spawnPoints;
+	public GameObject[] enemyTypes;
+	public int timeBeforeFirstWave = 5;
+	public int timeBetweenWaves = 30;
+	public int startingWaveSize = 3;
+
+	int waveNumber = 1;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("Spawn", timeBeforeFirstWave, timeBetweenWaves);
+	}
+
+	void Spawn () {
+		int numEnemies = startingWaveSize + waveNumber;
+
+		GameObject enemyType = enemyTypes [Random.Range (0, enemyTypes.Length)];
+		Transform spawnPoint = spawnPoints [Random.Range (0, spawnPoints.Length)];
+
+		print("Starting wave " + waveNumber + " with " + numEnemies + " enemies of type: " + enemyType.name);
+
+		//TODO instantiate at slightly different positions so they don't collide immmediately with each other
+		for (int i = 0; i < numEnemies; i++) {
+			Instantiate (enemyType, spawnPoint.position, spawnPoint.rotation);	
+		}
+
+		waveNumber++;
+	}
+}
